c983e8595b3e3d4a747b94d465adb34ea1a32d7c
caophuoclong <caophuoclong1@gmail.com>
2021-10-25 00:29:24 +0700

    udate

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,8 +1,8 @@
 const Contact = require("../models/contact.model");
 
 module.exports = {
-    create: async(req, res) => {
-        const { name, email, phone, address} = req.body;
+    create: async (req, res) => {
+        const { name, email, phone, address } = req.body;
         try {
             const contact = new Contact({
                 name,
@@ -24,7 +24,7 @@ module.exports = {
 
         }
     },
-    getBy: async(req, res) => {
+    getBy: async (req, res) => {
         const { phone, name } = req.query;
         console.log(req.query);
         const newRegex = new RegExp(name, "i");
@@ -42,22 +42,22 @@ module.exports = {
             });
         };
     },
-    getById: async (req,res) => {
+    getById: async (req, res) => {
         const { id } = req.params;
-        try{
+        try {
             const contact = await Contact.findById(id);
-        res.json({
-            status: "success",
-            data: contact,
-        })
-        }catch(error){
+            res.json({
+                status: "success",
+                data: contact,
+            })
+        } catch (error) {
             res.json({
                 status: "error",
                 message: error.message
             });
         }
     },
-    getAll: async(req, res) => {
+    getAll: async (req, res) => {
         const contacts = await Contact.find().sort({ create: -1 });
         res.json({
             status: "success",
@@ -65,44 +65,44 @@ module.exports = {
         });
     },
 
-    update: async(req, res) => {
-        const { name, email, phone, favorite } = req.body;
+    update: async (req, res) => {
+        const { name, email, phone, favorite, address } = req.body;
         const contact = await Contact.findOneAndUpdate({ phone: req.params.phone }, {
             name,
             email,
             phone,
-            favorite
+            favorite, address
         });
         res.json({
             status: "success",
             data: contact
         });
     },
-    favorite: async(req, res) => {
+    favorite: async (req, res) => {
         const { id } = req.params;
         const contact = await Contact.findById(id);
         contact.favorite = !contact.favorite;
         await contact.save();
-        if(contact.favorite){
+        if (contact.favorite) {
             res.json({
                 status: "success",
                 message: "Đã thêm liên hệ yêu thích!"
             });
-        }else{
+        } else {
             res.json({
                 status: "success",
                 message: "Đã xóa liên hệ yêu thích!"
             });
         }
     },
-    deleteByPhone: async(req, res) => {
-        const contact = await Contact.findByIdAndRemove(req.params.phone );
+    deleteByPhone: async (req, res) => {
+        const contact = await Contact.findByIdAndRemove(req.params.phone);
         res.json({
             status: "success",
             data: contact
         });
     },
-    deleteAll: async(req, res) => {
+    deleteAll: async (req, res) => {
         try {
             const contact = await Contact.deleteMany();
             res.json({
